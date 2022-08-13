6c0b4f8ea6a8cca476cbc1a4a95442f382299d29
Carji <51406858+Carji@users.noreply.github.com>
2022-08-13 10:44:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,11 +11,3 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
-echo "\
-[theme]\n\
-backgroundColor="#e3e3e6"\n\
-secondaryBackgroundColor="#a9a9ad"\n\
-textColor="#000000"\n\
-font="serif"\n\
-" > ~/.streamlit/config.toml
